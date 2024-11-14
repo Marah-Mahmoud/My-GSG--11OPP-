2fe25ada622913af697763d924aa9005ef703799
Marah Mahmoud <marymahmoud313@gmail.com>
2024-11-14 19:27:03 +0200

    player update

--- Assets/Scripts/Assignment13/GameManager01.cs
+++ Assets/Scripts/Assignment13/GameManager01.cs
@@ -10,16 +10,21 @@ public class GameManager01 : MonoBehaviour
         Player player01 = new Player();
         Player player02 = new Player();
 
-        player01.InitializePlayer("Jeniver",100);
+        player01.InitializePlayer("Jeniver",70);
         player02.InitializePlayer("Omar",80);
 
         Debug.Log("Player name is: " + player01.playerName + "  ,and her health is " + player01.health);
         Debug.Log("Player name is: " + player02.playerName + "  ,and his health is " + player02.health);
 
+        player01.Heal(30);
+        player01.Heal(true);
+        Debug.Log(player01.playerName + " health is " + player01.health + " when full restore true");
+
         player02.Heal(10);
         player02.Heal(true);
-        Debug.Log(player02.playerName + " health is " + player02.health);
+        Debug.Log(player02.playerName + " health is " + player02.health + " when full restore true");
 
+        
         Player.ShowPlayerCount();
      
 
